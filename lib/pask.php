4d0013c6bf7e2ec29a3d9ab676e8265f0533b3fc
hirakiuc <hirakiuc@gmail.com>
2009-11-02 11:08:00 +0900

    add simple comments.

--- lib/pask.php
+++ lib/pask.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
- *
+ * Abstract Task Class
  */
 class Pask{
-  /** */
+  /** task description strings. */
   public $desc = "";
 
-  /** */
+  /** Ths tasks depend on this task in ordered array.*/
   public $before_tasks = array(); 
 
-  /** */
+  /** need to implement task script in pure php scripts. */
   public function run(){}
 }
 
