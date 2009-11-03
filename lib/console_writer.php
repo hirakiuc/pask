70659155d7aa638e2a5bf8be802c558a1bc943c9
hirakiuc <hirakiuc@gmail.com>
2009-11-03 22:21:48 +0900

    add console_writer class

---
+++ lib/console_writer.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Abstract Message Writer Class
+ */
+abstract class Writer{
+  //---------------------------------
+  /** level:verbose */
+  public static $VERBOSE = 'verbose';
+  /** level:normal */
+  public static $NORMAL  = 'normal';
+  /** level:quiet */
+  public static $QUIET   = 'quiet'; 
+  /** level:debug (for developer) */
+  public static $DEBUG   = 'debug'; 
+  //---------------------------------
+
+  /** output level */
+  protected $level = null;
+
+  /** output destination (level:verbose,normal,debug) */
+  protected $output = null;
+
+  /** output destination (if error occured) */
+  protected $err_output = null;
+
+  /**
+   *
+   */
+  protected function __construct($output, $err_output, $level){
+    $this->level = $level;
+    $this->output   = $output;
+    $this->err_output = $err_output;
+  }
+
+  /**
+   *
+   */
+  abstract public static function getInstance($conf);
+
+  //----------------------------------------
+  /**  
+   *
+   */
+  public function verbose($str){
+    $this->puts($str);
+  }
+
+  /**  
+   *
+   */
+  public function debug($str){
+    $this->puts($str);
+  }
+
+  /**  
+   *
+   */
+  public function puts($str){
+    switch($this->level){
+      case Writer::$DEBUG:
+        fprintf($this->output, $this->debug_format($str) . "\n");
+        break;
+      case Writer::$VERBOSE:
+      case Writer::$NORMAL:
+        fprintf($this->output, $this->format($str) . "\n");
+        break;
+      case Writer::$QUIET:
+      default: 
+        break;
+    }
+  }
+
+  /**
+   *
+   */
+  public function error($str){
+    fprintf($this->err_output, $this->error_format($str) . "\n"); 
+  } 
+
+  //----------------------------------------
+  /**
+   * format the output string in Writer::Debug Level.
+   */
+  protected function debug_format($str){
+    return "DEBUG: " . $str;
+  }
+
+  /**
+   * format the output string.
+   *
+   * @param $str formated string.
+   */
+  protected function format($str){
+    return $str;
+  }
+
+  /**
+   * format the error output string.
+   *
+   * @param $str formated string.
+   */
+  protected function error_format($str){
+    return "ERROR: " . $str;
+  } 
+}
+
+
+/**
+ * Console Output Writer Class
+ */
+class ConsoleWriter extends Writer{
+
+  /**  */
+  private static $instance = null;
+
+  /**
+   * Get ConsoleWriter Instance
+   *
+   * @param array $conf array('level' => Writer::XXXX)
+   * @return ConsoleWriter Instance.
+   */
+  public static function getInstance($conf){
+    if(ConsoleLogger::$instance == null){
+      $this->instance = new ConsoleWriter(STDOUT, STDERR, $conf['level']);
+    } 
+    return $this->instance;
+  } 
+}
+
+?>
--- lib/include.php
+++ lib/include.php
@@ -1,10 +1,10 @@
 <?php
 
+require_once('console_writer.php');
 require_once('pask.php');
 require_once('errors.php');
 require_once('pask_loader.php');
 require_once('pask_runner.php'); 
 require_once('application.php');
 
-?>
-
+?> 
