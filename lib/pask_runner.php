83c89bffab41f888c9c3333a595cbcf90c14b40b
hirakiuc <hirakiuc@gmail.com>
2009-11-04 18:02:16 +0900

    change to use writer

--- lib/pask_runner.php
+++ lib/pask_runner.php
@@ -14,16 +14,21 @@ class PaskRunner{
   /** system config */
   private $conf = null;
 
+  /** output writer */
+  private $writer = null;
+
   /** map taskname with filepath */
   private $paskfile_map = null;
 
   /**
    * Constractor
    */
-  public function __construct($loader, $conf){ 
+  public function __construct($loader, $conf, $writer){ 
     $this->loader = $loader;
-    $this->paskfile_map = $loader->get_paskfile_map();
     $this->conf = $conf;
+    $this->writer = $writer; 
+
+    $this->paskfile_map = $loader->get_paskfile_map();
   }
 
   /**
@@ -32,28 +37,24 @@ class PaskRunner{
   public function run_task($task_name, $task_args){ 
     $stack = $loader->create_taskstack($task_name); 
 
-    echo "Run    '" . $task_name . "' task.\n";
+    ($this->writer)->puts("Run    '" . $task_name . "' task.");
 
     while(count($stack)!=0){
       try{ 
         $task_data = array_pop($stack); 
 
-        if($this->conf['debug']){
-          echo "Start '" . $task_data['task_name'] . "'\n";
-        }
+        ($this->writer)->verbose("Start '" . $task_data['task_name']);
 
         $task_data['pask']->run();
 
-        if($this->conf['debug']){ 
-          echo "End   '" . $task_data['task_name'] . "'\n";
-        }
+        ($this->writer)->verbose("End   '" . $task_data['task_name']);
 
       }catch(Exception $err){
         throw $err;
       }
     } 
 
-    echo "Finish '" . $task_name . "' task.\n";
+    ($this->writer)->puts("Finish '" . $task_name . "' task.");
   } 
 } 
 
