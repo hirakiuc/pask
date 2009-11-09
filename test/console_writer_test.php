8e6cb96cd8dc385493e5c0e8c8ec0b697b0d5b20
hirakiuc <hirakiuc@gmail.com>
2009-11-09 12:09:24 +0900

    add testcase files (testcase not implemented.)

---
+++ test/console_writer_test.php
@@ -0,0 +1,28 @@
+<?php
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2: */
+require_once(dirname(__FILE__)."/../lib/include.php");
+/**
+ * Writer/ConsoleWriter/TaskListWriter Test Class File
+ *
+ * PHP version 5
+ *
+ * @author hirakiuc <hirakiuc@gmail.com>
+ * @copyright 2009 pask project team
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version Git:$Id$
+ */
+require_once('PHPUnit/Framework.php');
+
+class WriterTest extends PHPUnit_Framework_TestCase{
+
+}
+
+class ConsoleWriterTest extends PHPUnit_Framework_TestCase{
+
+}
+
+class TaskListWriterTest extends PHPUnit_Framework_TestCase{
+
+}
+
+?>
---
+++ test/pask_loader_test.php
@@ -0,0 +1,21 @@
+<?php
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2: */
+require_once(dirname(__FILE__)."/../lib/include.php");
+/**
+ * Pask Loader Test Class File
+ *
+ * PHP version 5
+ *
+ * @author hirakiuc <hirakiuc@gmail.com>
+ * @copyright 2009 pask project team
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version Git:$Id$
+ */
+require_once('PHPUnit/Framework.php');
+
+class PaskLoaderTest extends PHPUnit_Framework_TestCase{
+
+}
+
+
+?>
