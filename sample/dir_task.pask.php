6a01241f3e1cdf6e2ac681425c21e481eb46bde6
hirakiuc <hirakiuc@localhost.localdomain>
2011-10-06 21:44:26 +0900

    merge sample paskfile

--- sample/dir_task.pask.php
+++ sample/dir_task.pask.php
@@ -1,11 +1,16 @@
 <?php
 /**
- * delete directory task
+ * directory task
  */
+
+desc("Directory Create task");
+task("create_dir", function() {
+  mkdir("/tmp/hogehoge");
+});
 
 desc("Directory delete task");
 task("delete_dir", array("create_dir"), function() {
   rmdir("/tmp/hogehoge");
-});
+}); 
 
 ?>
--- sample/file_task.pask.php
+++ sample/file_task.pask.php
@@ -8,4 +8,10 @@ task("create_file", function() {
   touch("/tmp/hogehoge");
 });
 
+desc("File delete task");
+task("delete_file", array("create_file"), function() {
+  unlink("/tmp/hogehoge");
+});
+
+
 ?>
